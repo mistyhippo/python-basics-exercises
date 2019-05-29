3a7caa93c50a14c8d097f2f56dda5c55a414685e
mistyhippo <Valentinabrent@gmail.com>
2019-05-28 21:44:21 -0700

    basics, flow control, functions

---
+++ hello.py
@@ -0,0 +1,9 @@
+#Python Basics
+# exercises going over python basics  (# is how you preface comments)
+
+
+# This program says hello and asks for my name.
+
+print('Hello World')
+print('What is your name')  # asks for their name
+
---
+++ helloFunc.py
@@ -0,0 +1,16 @@
+def hello():
+  print('Howdy!')
+  print('Howdy!!!')
+  print('Hello there.')
+hello()
+hello()
+hello()
+
+
+#def statements with parameters
+
+def hello(name):
+  print('Hello ' + name)
+
+hello('Alice')
+hello('Bob')
